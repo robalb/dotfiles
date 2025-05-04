6c95e5ab9d3b42f9fc2c1ad939e13de22f3c7713
robalb <11277482+robalb@users.noreply.github.com>
2025-05-04 23:38:08 +0200

    Add lsp shortcuts

--- .config/nvim_chad/lua/mappings.lua
+++ .config/nvim_chad/lua/mappings.lua
@@ -32,11 +32,30 @@ map("n", "<leader><leader>", "/", {desc="/ search"})
 map("n", "<leader>>", "<C-W>15>", {desc="vertical resize increase"})
 map("n", "<leader><", "<C-W>15<", {desc="vertical resize decrease"})
 
+-- LSP rename
+map("n", "<leader>r", require "nvchad.lsp.renamer", {desc="LSP rename"})
+-- LSP code action
+map("n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", {desc="LSP code action"})
+-- LSP show line error in float box
+map("n", "<leader>l", "<cmd>lua vim.diagnostic.open_float()<CR>", {desc="LSP show line error"})
+-- LSP signature / hover
+map("n", "<leader>s", "<cmd>lua vim.lsp.buf.hover()<CR>", {desc="LSP signature / hover"})
+-- LSP next error
+map("n", "<leader>n", "<cmd>lua vim.diagnostic.goto_next()<CR>", {desc="error jump next"})
+-- LSP prev error
+map("n", "<leader>N", "<cmd>lua vim.diagnostic.goto_prev()<CR>", {desc="error jump prev"})
+
+
 -- Disable mappings
 local nomap = vim.keymap.del
 
 -- normally this would open the theme explorer,
 -- but it makes the <leader>t command slower.
 nomap("n", "<leader>th")
+-- normally this would open the renamer, but we moved it to <leader>r
+-- nomap("n", "<leader>ra") --BUG, cannot unmpa
+-- normally this would toggle the relative line number, we dont use this.
+nomap("n", "<leader>rn")
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
