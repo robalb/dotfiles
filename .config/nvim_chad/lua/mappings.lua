e16e5169d60e34db8bd95b2b53ddd063c3aca94d
robalb <11277482+robalb@users.noreply.github.com>
2025-05-04 16:33:59 +0200

    Add nvchad

---
+++ .config/nvim_chad/lua/mappings.lua
@@ -0,0 +1,42 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+map({ 'n', 't', 'v', 'i' }, "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+map({ 'n', 't', 'v', 'i' }, "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+map({ 'n', 't', 'v', 'i' }, "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+map({ 'n', 't', 'v', 'i' }, "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+
+map("n", "<leader>t", "<cmd>Telescope live_grep<CR>", { desc = "Telescope live grep" })
+map("n", "<leader>h", ":noh<CR>", { desc = "Cancel highlight" })
+
+-- Multiline word replacement:
+-- shortcut for:
+-- - searching a word
+-- - changing it with cgn
+-- - pressing dot to
+-- repeat the action on the next word
+map("n", "<leader>c", "*Ncgn", { desc = "Multiline word replacement" })
+-- (this action was overwritten by our <leader>c)
+map("n", "<leader>gc", "<cmd>Telescope git_commits<CR>", {desc = "telescope git commit"})
+-- (this action was originally <leader>th but we disabled it)
+map("n", "<leader>ft", "<cmd>Telescope themes<CR>", {desc = "telescope nvchad themes"})
+-- fast access to / on non-US keyboards
+map("n", "<leader><leader>", "/", {desc="/ search"})
+map("n", "<leader>>", "<C-W>15>", {desc="vertical resize increase"})
+map("n", "<leader><", "<C-W>15<", {desc="vertical resize decrease"})
+
+-- Disable mappings
+local nomap = vim.keymap.del
+
+-- normally this would open the theme explorer,
+-- but it makes the <leader>t command slower.
+nomap("n", "<leader>th")
+
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
---
+++ .config/nvim_chad/lua/plugins/init.lua
@@ -0,0 +1,72 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+    'alexghergh/nvim-tmux-navigation',
+    lazy = false, -- ensures it loads on startup
+    config = function()
+      require 'nvim-tmux-navigation'.setup {
+        disable_when_zoomed = true, -- defaults to false
+        keybindings = {
+          left = "<C-h>",
+          down = "<C-j>",
+          up = "<C-k>",
+          right = "<C-l>",
+          last_active = "<C-\\>",
+          next = "<C-Space>",
+        }
+      }
+    end,
+  },
+
+  {
+    "karb94/neoscroll.nvim",
+    -- lazy load the first time one of these keys is pressed:
+    keys = {
+      { "<C-u>", nil},
+      { "<C-d>", nil},
+      { "<C-b>", nil},
+      { "<C-f>", nil},
+    },
+    opts = {},
+    config = function()
+      require('neoscroll').setup({
+        -- All these keys will be mapped to their corresponding default scrolling animation
+        mappings = { '<C-u>', '<C-d>', '<C-b>', '<C-f>', 'zt', 'zz', 'zb' },
+        hide_cursor = true,
+        stop_eof = true,
+        use_local_scrolloff = false,
+        respect_scrolloff = false,
+        cursor_scrolls_alone = true,
+        easing = "quadratic",
+        pre_hook = nil,
+        post_hook = nil,
+      })
+    end
+  },
+
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
+
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
